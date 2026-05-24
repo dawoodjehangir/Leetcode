3ae29afa3f3865d80a1cddeee1b3fea1a04ff0e7
Dawood <dawood.jehangir@gmail.com>
2026-05-25 00:39:53 +0200

    q374 revisited

--- revisiting250.ts
+++ revisiting250.ts
@@ -631,3 +631,21 @@ function hasCycle(head: ListNode | null): boolean {
   }
   return false;
 }
+
+//374. Guess Number Higher or Lower
+function guessNumber(n: number): number {
+  let low: number = 1;
+  let high: number = n;
+  while (low <= high) {
+    let mid: number = low + Math.floor((high - low) / 2);
+    const result = guess(mid);
+    if (result === 0) {
+      return mid;
+    } else if (result === 1) {
+      low = mid + 1;
+    } else if (result === -1) {
+      high = mid - 1;
+    }
+  }
+  return -1;
+}
