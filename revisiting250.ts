ccd49bb8b4fd4c68249117b5c92a08d63b544975
Dawood <dawood.jehangir@gmail.com>
2026-06-09 00:47:01 +0200

    merge two sorted lists revisited

--- revisiting250.ts
+++ revisiting250.ts
@@ -838,3 +838,29 @@ function reverseList(head: ListNode | null): ListNode | null {
   reverseRecursion(head, null);
   return head;
 }
+
+//21. Merge Two Sorted Lists
+function mergeTwoLists(
+  list1: ListNode | null,
+  list2: ListNode | null,
+): ListNode | null {
+  const newHead: ListNode | null = new ListNode(-1, null);
+  let temp = newHead;
+  while (list1 !== null && list2 !== null) {
+    if (list1.val <= list2.val) {
+      temp.next = list1;
+      list1 = list1.next;
+    } else {
+      temp.next = list2;
+      list2 = list2.next;
+    }
+    temp = temp.next;
+  }
+  if (list1 !== null) {
+    temp.next = list1;
+  }
+  if (list2 !== null) {
+    temp.next = list2;
+  }
+  return newHead.next;
+}
