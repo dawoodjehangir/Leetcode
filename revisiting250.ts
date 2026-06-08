0f372f90d2ce2931f02ab9ef89be042c286c4cd4
Dawood <dawood.jehangir@gmail.com>
2026-06-09 01:44:56 +0200

    valid palindrome

--- revisiting250.ts
+++ revisiting250.ts
@@ -878,3 +878,33 @@ function hasCycle(head: ListNode | null): boolean {
   }
   return false;
 }
+
+//125. Valid Palindrome
+
+function isPalindrome(s: string): boolean {
+  let l: number = 0;
+  let r: number = s.length - 1;
+  const isAlphanumeric = (s: string): boolean => {
+    if (
+      (s >= "a" && s <= "z") ||
+      (s >= "A" && s <= "Z") ||
+      (s >= "0" && s <= "9")
+    )
+      return true;
+    return false;
+  };
+  while (l <= r) {
+    while (l < r && !isAlphanumeric(s[l])) {
+      l++;
+    }
+    while (l < r && !isAlphanumeric(s[r])) {
+      r--;
+    }
+    if (s[l].toLowerCase() !== s[r].toLowerCase()) {
+      return false;
+    }
+    l++;
+    r--;
+  }
+  return true;
+}
